a94070680c8039cb7002cccd1fc4eba5dabd15cd
HuyenNguyen <huyenthingocnguyen@kms-technology.com>
2021-01-11 16:48:42 +0700

    init dynamic-test-suite-sample project (cont.)

---
+++ Test Suites/DTS_Verify Successful Login and Appointment.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>DTS_Verify Successful Login and Appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>33e4796c-b056-4fc2-a3e8-87663e260797</testSuiteGuid>
+   <filteringExtension>com.katalon.plugin.tags.AdvancedTagQueryingTestSuiteImpl</filteringExtension>
+   <filteringPlugin>com.katalon.tags-plugin</filteringPlugin>
+   <filteringText>tags=(login)</filteringText>
+</FilteringTestSuiteEntity>
